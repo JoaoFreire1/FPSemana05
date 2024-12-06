4ccc8e98a86c454bda65a3e9b63252876ec77663
JoaoFreire1 <joao.freire.art@gmail.com>
2024-12-06 10:59:36 +0000

    Exercise complete

---
+++ banco.py
@@ -0,0 +1,35 @@
+
+
+class ContaBancaria:
+
+    def __init__(self, titular, saldo, limite):
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def depositar(self, valor):
+        if valor > 0 :
+            print("1")
+            self.saldo = self.saldo + valor
+        else:
+            print("0")
+
+    def levantar(self, valor):
+        if self.limite + self.saldo > valor:
+            self.saldo = self.saldo - valor
+            print("1")
+        else:
+            print("0")
+        
+    def exibir_saldo(self):
+        print(f"{self.saldo:.2f}")
+
+    def exibir_info(self):
+        print(f"{self.titular} {self.saldo:.2f} {self.limite:.2f}")
+
+conta = ContaBancaria("João", 1000.00, 500.00)
+conta.depositar(-500.00)
+conta.depositar(500.00)
+conta.levantar(1200.00)
+conta.levantar(1200.00)
+conta.exibir_info()
